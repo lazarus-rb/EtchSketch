041616bf981e96279de70689076f836a9b2c85d5
Laz <114888437+lazarus-rb@users.noreply.github.com>
2025-07-29 14:20:22 +1200

    Pixel darkening implemented, gradually fades to black over 10 steps/hovers

--- script.js
+++ script.js
@@ -6,18 +6,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const pixelDiv = document.createElement("div");
     pixelDiv.classList.add('pixel-div');
+    pixelDiv.style.backgroundColor = 'rgb(255,254,253)';
     containerDiv.appendChild(pixelDiv);
+    
+    
+    pixelDiv.addEventListener("mouseover", darkenPixel);
 
 
     function darkenPixel(div) {
         let divToDarken = div;
-        divToDarken.target.style.backgroundColor = 'red';
-        // let { hue, saturation, lightness } = getHslComponents(divToDarken.target.style.backgroundColor);
-        // console.log(divToDarken.target.style.backgroundColor);
+        let divColor = divToDarken.target.style.backgroundColor;
+
+        let parseColor = divColor.match(/\d+/g); //returns array of individual r/g/b values as strings
+        
+        let rgbColor = parseColor.map(element => parseInt(element, 10));
+        
+        rgbColor.forEach((element, index) => {
+            if(rgbColor[index] > 25) {
+                rgbColor[index] -= 25;
+            }
+            else rgbColor[index] = 0;
+            });
+
+        let newColor = 'rgb(' + rgbColor[0] + ", " + rgbColor[1] + ", " + rgbColor[2] + ")";
+
+        divToDarken.target.style.backgroundColor = newColor;
+        
     }
-
-
-    pixelDiv.addEventListener("mouseover", darkenPixel);
 
 
 
