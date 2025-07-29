2daa5a0a8b0f7cba5ed2c79ef6b529c89e113df2
Laz <114888437+lazarus-rb@users.noreply.github.com>
2025-07-29 17:55:17 +1200

    reset implemented with user specificed grid dynamic scale grid

--- script.js
+++ script.js
@@ -4,14 +4,62 @@ document.addEventListener('DOMContentLoaded', () => {
     containerDiv.classList.add("main-container");
     document.body.appendChild(containerDiv);
 
-    const pixelDiv = document.createElement("div");
-    pixelDiv.classList.add('pixel-div');
-    pixelDiv.style.backgroundColor = 'rgb(255,254,253)';
-    containerDiv.appendChild(pixelDiv);
+    const pixelContainer = document.createElement("div");
+    pixelContainer.classList.add("pixel-container");
     
-    
-    pixelDiv.addEventListener("mouseover", darkenPixel);
+    containerDiv.appendChild(pixelContainer);
 
+    const buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("button-div");
+    containerDiv.appendChild(buttonDiv);
+
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Reset!";
+    resetButton.addEventListener('click', resetClicked);
+    buttonDiv.appendChild(resetButton);
+
+    function resetClicked() {
+        let newGridSize = prompt('Please enter a new grid size: ');
+        console.log(newGridSize);
+
+        if(newGridSize > 0) {
+            while(pixelContainer.firstChild) {
+                pixelContainer.removeChild(pixelContainer.firstChild);
+            }
+            createGrid(newGridSize);
+
+        }
+
+        else {
+            alert("Grid size should probably be a number");
+            return null;
+        }
+    }
+        
+
+    function createGrid(gridSize) {
+            let pixelWidth =  1/gridSize*100;  
+
+            for(let i = 0; i<gridSize; i++) {
+                    for(let y = 0; y<gridSize; y++) {
+                    const pixelDiv = document.createElement("div");
+                    pixelDiv.classList.add('pixel-div');
+                    pixelDiv.style.backgroundColor = 'rgb(255,254,253)';
+                    
+                    
+                    pixelDiv.style.width = `${pixelWidth}%`;
+
+                    pixelDiv.style.height = pixelDiv.style.width;
+
+                    pixelContainer.appendChild(pixelDiv);
+                    pixelDiv.addEventListener("mouseover", darkenPixel);
+                }
+        }
+    }
+
+    let pixelRowCount = 50; //Default grid size on page load
+
+    createGrid(pixelRowCount);
 
     function darkenPixel(div) {
         let divToDarken = div;
